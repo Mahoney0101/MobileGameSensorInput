f326cca4e7b6e64beba63ba4d81e1de9359a0515
Mahoney0101 <hoooch.mahoney@gmail.com>
2021-02-14 13:38:30 +0000

    fix panning bug

--- Assets/TouchManagerScript.cs
+++ Assets/TouchManagerScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TouchManagerScript : MonoBehaviour
 {
-    Vector2 lastTouchPosition;
     IControllable selectedObject;
     private float tapBegan;
     private bool tapMoved;
@@ -17,11 +16,7 @@ public class TouchManagerScript : MonoBehaviour
     Camera cam;
     const float pinchTurnRatio = Mathf.PI / 2;
     const float minTurnAngle = 0;
-    const float pinchRatio = 1;
-
     static public float turnAngle;
-    private Vector3 initialScale;
-    private float initialDistance;
 
     void Start()
     {
@@ -30,6 +25,69 @@ public class TouchManagerScript : MonoBehaviour
 
     void Update()
     {
+    if(Input.touchCount == 1 && selectedObject == null)
+        {
+            Touch touch = Input.GetTouch(0);
+            cam.transform.Translate(touch.deltaPosition * -0.01f); 
+        }
+    if (Input.touchCount == 1) 
+    {
+        Touch touch = Input.touches[0];
+        RaycastHit info;
+        Ray ourRay = Camera.main.ScreenPointToRay(touch.position);
+        Debug.DrawRay(ourRay.origin, 30 * ourRay.direction);
+        if (touch.phase == TouchPhase.Began)
+        {
+            tapBegan = Time.time;
+            tapMoved = false;
+        }
+
+        if (touch.phase == TouchPhase.Moved)
+        {
+            tapMoved = true;
+
+        }
+        if (touch.phase == TouchPhase.Ended && tapMoved == false)
+        {
+            float tapLength = Time.time - tapBegan;
+            if (tapLength <= tapTime && Physics.Raycast(ourRay, out info))
+            {
+                IControllable object_hit = info.transform.GetComponent<IControllable>();
+                if(object_hit != null)
+                {
+                    if(selectedObject == null){
+                        selectedObject = object_hit;
+                        Renderer renderer = info.transform.GetComponent<Renderer>();
+                        renderer.material.SetColor("_Color",Color.red);
+                        starting_distance_to_selected_object = Vector3.Distance(Camera.main.transform.position, info.transform.position);
+                    }
+                    else{
+                        Renderer renderer = info.transform.GetComponent<Renderer>();
+                        renderer.material.SetColor("_Color",Color.white);
+                        selectedObject = null;
+                    }
+                }
+            }
+        }
+        if(selectedObject != null){
+        switch(Input.touches[0].phase)
+        {
+            case TouchPhase.Began:
+                break;
+            case TouchPhase.Moved:
+                new_position = Camera.main.ScreenPointToRay(Input.touches[0].position);
+                selectedObject.MoveTo(ourRay, Input.touches[0], new_position.GetPoint(starting_distance_to_selected_object));
+                break;
+            case TouchPhase.Stationary:
+                // new_position = Camera.main.ScreenPointToRay(Input.touches[0].position);
+                // selectedObject.MoveTo(Input.touches[0],new_position.GetPoint(starting_distance_to_selected_object));
+                break;
+            case TouchPhase.Ended:
+                selectedObject.Stop();
+                break;
+        }
+        }
+    }
     if (Input.touchCount == 2 && selectedObject == null)
     {
                 Touch tZero = Input.GetTouch(0);
@@ -55,85 +113,13 @@ public class TouchManagerScript : MonoBehaviour
                     }
     }
 
-    
-    if (Input.touchCount == 1) 
-    {
-        Touch touch = Input.touches[0];
-        RaycastHit info;
-        Ray ourRay = Camera.main.ScreenPointToRay(touch.position);
-        Debug.DrawRay(ourRay.origin, 30 * ourRay.direction);
-        if (touch.phase == TouchPhase.Began)
-        {
-            tapBegan = Time.time;
-            tapMoved = false;
-        }
-        if (touch.phase == TouchPhase.Moved)
-        {
-            tapMoved = true;
-        }
-        if (touch.phase == TouchPhase.Ended && tapMoved == false)
-        {
-            float tapLength = Time.time - tapBegan;
-            if (tapLength <= tapTime && Physics.Raycast(ourRay, out info))
-            {
-                IControllable object_hit = info.transform.GetComponent<IControllable>();
-                if(object_hit != null)
-                {
-                    if(selectedObject == null){
-                        Debug.Log("YOUVE BEEN TAPPED");
-                        selectedObject = object_hit;
-                        Renderer renderer = info.transform.GetComponent<Renderer>();
-                        renderer.material.SetColor("_Color",Color.red);
-                        starting_distance_to_selected_object = Vector3.Distance(Camera.main.transform.position, info.transform.position);
-                    }
-                    else{
-                        Renderer renderer = info.transform.GetComponent<Renderer>();
-                        renderer.material.SetColor("_Color",Color.white);
-                        selectedObject = null;
-                        Debug.Log("Deselected object");
-                    }
-                }
-            }
-        }
-        //assume selected object is not null. object selected. drag code here
-        if(selectedObject != null){
-        switch(Input.touches[0].phase)
-        {
-            case TouchPhase.Began:
-                break;
-            case TouchPhase.Moved:
-                new_position = Camera.main.ScreenPointToRay(Input.touches[0].position);
-                selectedObject.MoveTo(ourRay, Input.touches[0], new_position.GetPoint(starting_distance_to_selected_object));
-                break;
-            case TouchPhase.Stationary:
-                // new_position = Camera.main.ScreenPointToRay(Input.touches[0].position);
-                // selectedObject.MoveTo(Input.touches[0],new_position.GetPoint(starting_distance_to_selected_object));
-                break;
-            case TouchPhase.Ended:
-                selectedObject.Stop();
-                break;
-        }
-        }
-    }
     if(Input.touchCount == 2 && selectedObject != null)
      {
-        Touch touch1 = Input.GetTouch(0);
-        Touch touch2 = Input.GetTouch(1);
         selectedObject.RotateObject(Calculate());
         selectedObject.ScaleObject();
      }
     
 }
-
-	const float minPinchDistance = 0;
- 
-	const float panRatio = 1;
-	const float minPanDistance = 0;
- 
-	static public float pinchDistanceDelta;
-
-	static public float pinchDistance;
-
     /*
     Calculate Method
     Author: Caue Rego (cawas) 
@@ -141,7 +127,6 @@ public class TouchManagerScript : MonoBehaviour
     */
 	static public float Calculate () {
         float turnAngleDelta;
-		pinchDistance = pinchDistanceDelta = 0;
 		turnAngle = turnAngleDelta = 0;
  
 		if (Input.touchCount == 2) {
